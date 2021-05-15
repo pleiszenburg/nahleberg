f679343b25dd1b365f28c178de38427bd35a9352
Sebastian M. Ernst <ernst@pleiszenburg.de>
2021-05-15 19:47:16 +0200

    translations

---
+++ i18n/nahleberg_en.ts
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="en" sourcelanguage="en">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../nahleberg/_core/msg.py" line="48"/>
+        <source>Critical error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/msg.py" line="54"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/msg.py" line="61"/>
+        <source>Internal error. No description can be provided. Please file a bug!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/config.py" line="54"/>
+        <source>QGIS settings path does not point to a directory.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/config.py" line="56"/>
+        <source>QGIS settings path does not exist.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/config.py" line="60"/>
+        <source>QGIS plugin configuration path exists but is not a directory.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/config.py" line="191"/>
+        <source>Parent of &quot;fn&quot; must exists.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/config.py" line="193"/>
+        <source>Parent of &quot;fn&quot; must be a directory.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/config.py" line="87"/>
+        <source>&quot;fn&quot; must be a file. (config)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/config.py" line="93"/>
+        <source>Config does not contain valid JSON.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/config.py" line="95"/>
+        <source>Configuration data must be a dict.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/config.py" line="101"/>
+        <source>Unknown configuration field &quot;name&quot;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/config.py" line="195"/>
+        <source>&quot;value&quot; contains not allowed types.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/config.py" line="158"/>
+        <source>Could not backup old configuration before saving new - too many old backups.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/config.py" line="172"/>
+        <source>&quot;fn&quot; must exists.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/config.py" line="174"/>
+        <source>&quot;fn&quot; must be a file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/config.py" line="182"/>
+        <source>&quot;fn&quot; does not contain valid JSON.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/nahleberg.py" line="64"/>
+        <source>plugin_root_fld must exists</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/nahleberg.py" line="66"/>
+        <source>plugin_root_fld must be a directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/nahleberg.py" line="92"/>
+        <source>&amp;Nahleberg Management</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../nahleberg/_core/nahleberg.py" line="98"/>
+        <source>Nahle&amp;berg</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
